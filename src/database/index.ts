29e7408d932874b6c975c1be59f4970311a5aaf3
Wilian Ansanello <wiliansanello@outlook.com>
2023-03-07 20:54:34 -0300

    error on insert data in a postgresql docker container

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,15 +1,6 @@
-import { createConnection, getConnectionOptions } from "typeorm";
+import { createConnection } from "typeorm";
 
-interface IOptions {
-    host: string;
-}
-
-getConnectionOptions().then(options => {
-    const newOptions = options as IOptions;
-    newOptions.host = "database";
-    createConnection({
-        ...options,
-    });
-});
+console.log("Arquivo database");
+createConnection();
 
 
